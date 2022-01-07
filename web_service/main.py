d4f96555178f27b01a0b6d3167278ec8cc146ed4
esmaksimov <esmaksimov@gmail.com>
2022-01-07 23:30:32 +0300

    main_functionality: added database, some routes separation etc

--- web_service/main.py
+++ web_service/main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI
 import uvicorn
+from routers import users, main
 
 app = FastAPI()
-
-
-@app.get("/")
-async def main_page():
-    pass
+app.include_router(main.router)
+app.include_router(users.router, prefix='/users', tags=['users'])
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
