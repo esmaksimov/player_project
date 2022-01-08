8f2126004ea8011ccf9838c71f967c2011b5953e
esmaksimov <esmaksimov@gmail.com>
2022-01-08 23:52:58 +0300

    main_functionality: initial code for static files

--- web_service/main.py
+++ web_service/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 import uvicorn
 from routers import users, main
+from pathlib import Path
 
 app = FastAPI()
 app.include_router(main.router)
 app.include_router(users.router, prefix='/users', tags=['users'])
+app.mount(
+    "/static",
+    StaticFiles(directory=(Path(__file__).parent / 'static').resolve()),
+    name="static"
+)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
