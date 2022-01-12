ef974dd55cb2535484d94e0b228b9e85d263f2e5
esmaksimov <esmaksimov@gmail.com>
2022-01-13 01:50:44 +0300

    main_functionality: oauth2 scheme initial code and pydantic model User

--- web_service/routers/users.py
+++ web_service/routers/users.py
@@ -1,3 +1,11 @@
 from fastapi import APIRouter
+from fastapi.security import OAuth2PasswordBearer
 
 router = APIRouter()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth")
+
+
+@router.get("/auth/")
+async def auth_user():
+    pass
